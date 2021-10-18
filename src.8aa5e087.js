93d2653d590d3efdc0236b71de5a416843521950
mryevhenii07 <65964265+mryevhenii07@users.noreply.github.com>
2021-10-18 18:53:21 +0000

    Deploying to gh-pages from @ mryevhenii07/new-project@46262586015fb4eec716c7054157ac84f2217154 🚀

--- src.8aa5e087.js
+++ src.8aa5e087.js
@@ -3,4 +3,4 @@ parcelRequire=function(e,r,t,n){var i,o="function"==typeof parcelRequire&&parcel
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss");
 },{"./sass/main.scss":"clu1"}]},{},["Focm"], null)
-//# sourceMappingURL=/new-project/src.0a5a0aaf.js.map
+//# sourceMappingURL=/new-project/src.8aa5e087.js.map
