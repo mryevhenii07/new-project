6ee36968301f9111574d8749a6206353c22bb50c
Yurii <ivaschenko_u@ukr.net>
2021-10-20 12:49:43 +0300

    add script

---
+++ src/mobile-menu.js
@@ -0,0 +1,12 @@
+const menurefs = {
+    openMenuBtn: document.querySelector('[data-menu-open]'),
+    closeMenuBtn: document.querySelector('[data-menu-close]'),
+    menuBackdrop: document.querySelector('[data-menu]'),
+  };
+
+  menurefs.openMenuBtn.addEventListener('click', toggleMenu);
+  menurefs.closeMenuBtn.addEventListener('click', toggleMenu);
+
+  function toggleMenu() {
+    menurefs.menuBackdrop.classList.toggle('visually-hidden');
+  }
