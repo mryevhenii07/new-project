ab6c18f47f711c297ad7cf915923fb844e1725d4
Yurii <ivaschenko_u@ukr.net>
2021-10-20 16:38:17 +0300

    add mobmenu and novigation

--- src/mobile-menu.js
+++ src/mobile-menu.js
@@ -2,8 +2,9 @@ const menurefs = {
     openMenuBtn: document.querySelector('[data-menu-open]'),
     closeMenuBtn: document.querySelector('[data-menu-close]'),
     menuBackdrop: document.querySelector('[data-menu]'),
+    mobMenulist: document.querySelector('.mob-menu-list'),
   };
-
+console.log(menurefs)
   menurefs.openMenuBtn.addEventListener('click', toggleMenu);
   menurefs.closeMenuBtn.addEventListener('click', toggleMenu);
 
@@ -11,4 +12,10 @@ const menurefs = {
 
     menurefs.menuBackdrop.classList.toggle('is-hidden');
 
+  }
+  menurefs.mobMenulist.addEventListener('click', hiddenMenu);
+  function hiddenMenu (even) {
+  
+    if(!even.target.classList.contains("mob-menu-link")) return
+    menurefs.menuBackdrop.classList.add('is-hidden');
   }
