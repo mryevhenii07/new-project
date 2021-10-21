757ff3c88afecbe47075b5a8b2a22564de6ad6a0
mryevhenii07 <65964265+mryevhenii07@users.noreply.github.com>
2021-10-21 11:19:49 +0000

    Deploying to gh-pages from @ mryevhenii07/new-project@ff4a8c0c515127b16d274f2feffe739136ce2746 🚀

--- src.cceed905.js
+++ src.cceed905.js
@@ -9,4 +9,4 @@ class i{constructor(i){this.page=$(i),this.topLink=this.page.find(".topLink"),$(
 },{}],"Focm":[function(require,module,exports) {
 "use strict";require("./sass/main.scss"),require("./carousel.js"),require("./mobile-menu.js"),require("./scripts/scroll.js"),(new WOW).init();
 },{"./sass/main.scss":"clu1","./carousel.js":"VYrc","./mobile-menu.js":"Cz5k","./scripts/scroll.js":"QN18"}]},{},["Focm"], null)
-//# sourceMappingURL=/new-project/src.2a3300e8.js.map
+//# sourceMappingURL=/new-project/src.cceed905.js.map
